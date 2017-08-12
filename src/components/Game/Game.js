848364c07f321dba066bc2370e33b66b79a5285f
loren pabst <lorenpabst@gmail.com>
2017-08-12 11:01:32 -0600

    added rules component and made home component into game component

---
+++ src/components/Game/Game.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './Game.css';
+
+
+class Game extends Component {
+
+  render() {
+    return (
+      <div className="game">
+
+          Game page
+
+      </div>
+    );
+  }
+}
+
+
+export default Game;
---
+++ src/components/Rules/Rules.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './Rules.css';
+
+
+class Rules extends Component {
+
+  render() {
+    return (
+      <div className="rules">
+
+          Gamepage
+
+      </div>
+    );
+  }
+}
+
+
+export default Rules;
