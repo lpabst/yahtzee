4de3c1e59353265da90c2995d2b18e0939b1dd0d
loren pabst <lorenpabst@gmail.com>
2017-08-12 12:15:33 -0600

    added scoring tables to scoresheet

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Yahtzee
 
-This is a work in progress, I'm building a yahtzee game in React
+This is a work in progress, I'm building a yahtzee game in React. I built it for a desktop, so it isn't mobile responsive.
 
 
 
--- src/components/Game/Scoresheet/Scoresheet.js
+++ src/components/Game/Scoresheet/Scoresheet.js
@@ -9,7 +9,75 @@ class Scoresheet extends Component {
     return (
       <div className="scoresheet">
 
-          Scoresheet 
+          <h2>Scoresheet</h2>
+
+          <h3>Upper</h3>
+          <table>
+            <tr>
+              <th>1</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>2</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>3</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>4</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>5</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>6</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Bonus</th>
+            </tr>
+            <tr>
+              <th>Total</th>
+            </tr>
+          </table>
+
+          <h3>Lower</h3> 
+          <table>
+            <tr>
+              <th>3-of-a-kind</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>4-of-a-kind</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Full House</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Small Straight</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Large Straight</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Yahtzee</th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Chance</th>
+              <th></th>
+            </tr>
+          </table>
+
+          <h3>Total</h3>
 
       </div>
     );
